2440e41900859cebc7c81efd00f249c303ca5df9
Notorioussensei <52688428+Masterkent11@users.noreply.github.com>
2023-06-26 20:14:34 -0700

    update new data api

--- myapp/src/api/data.js
+++ myapp/src/api/data.js
@@ -11,5 +11,30 @@ export const people = [
     name: "Jane Doe",
     details: "Some details about Jane",
   },
+  {
+    id: 2,
+    name: "Millie Bobby Brown",
+    details: "Stranger things actress",
+  },
+  {
+    id: 3,
+    name: "Jimmy Butler",
+    details: "NBA player",
+  },
+  {
+    id: 5,
+    name: "Nikola Jokic",
+    details: "NBA player",
+  },
+  {
+    id: 8,
+    name: "Kent",
+    details: "testing",
+  },
+  {
+    id: 9,
+    name: "Austin",
+    details: "Nurse",
+  },
   // More people here...
 ];

9abf80bc7a642c2605fa04f63b5f22f16960e2da
Notorioussensei <52688428+Masterkent11@users.noreply.github.com>
2023-06-28 17:04:44 -0700

    update person.js

--- myapp/src/store/person.js
+++ myapp/src/store/person.js
@@ -1,25 +1,30 @@
-const persons = {
-  state: {
-    people: [],
+// store/person.js
+import { people } from "../api/data.js";
+
+const state = {
+  people: people,
+};
+
+const mutations = {
+  updatePerson(state, updatedPerson) {
+    const person = state.people.find((p) => p.id === updatedPerson.id);
+    if (person) {
+      person.name = updatedPerson.name;
+      person.details = updatedPerson.details;
+    }
   },
-  mutations: {
-    updatePerson(state, updatePerson) {
-      const person = state.people.find((p) => p.id === updatePerson.id);
-      if (person) {
-        person.name = updatePerson.name;
-        person.details = updatePerson.details;
-      }
-    },
-    deletePerson(state, personId) {
-      const index = state.findIndex((p) => p.id === personId);
-      if (index !== -1) {
-        state.people.splice(index, 1);
-      }
-    },
-    addPerson(state, newPerson) {
-      state.people.push(newPerson);
-    },
+  deletePerson(state, personId) {
+    const index = state.people.findIndex((p) => p.id === personId);
+    if (index !== -1) {
+      state.people.splice(index, 1);
+    }
+  },
+  addPerson(state, newPerson) {
+    state.people.push(newPerson);
   },
 };
 
-export default persons;
+export default {
+  state,
+  mutations,
+};

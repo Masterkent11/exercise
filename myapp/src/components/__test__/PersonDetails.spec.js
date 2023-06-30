0cc8864f3bf24ecc59ddaf4d93af3c09be5fd4e0
Notorioussensei <52688428+Masterkent11@users.noreply.github.com>
2023-06-29 20:53:02 -0700

    UnitTest/SUPPORT-1-PesonDetails-test

--- myapp/src/components/__test__/PersonDetails.spec.js
+++ myapp/src/components/__test__/PersonDetails.spec.js
@@ -1,6 +1,6 @@
-import { mount } from "@vue/test-utils";
-import PersonDetails from "@/components/PersonDetails.vue";
-import { people } from "@/api/data.js";
+import { mount } from "vue/test-utils";
+import PersonDetails from "../PersonDetails/PersonDetails.vue";
+import { people } from "../../api/data";
 
 describe("PersonDetails", () => {
   let wrapper;
